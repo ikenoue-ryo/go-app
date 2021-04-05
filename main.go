1e900295abc6ecbecd95a6d9d85843db59189cb3
ikenoue-ryo <ikenoue.ryo@gmail.com>
2021-04-06 06:53:45 +0900

    APP-11 ユーザー取得

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test3"
-	u.Email = "test3@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
+	// u := &models.User{}
+	// u.Name = "test3"
+	// u.Email = "test3@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
 
-	u.CreateUser()
+	// u.CreateUser()
 
 	// u, _ := models.GetUser(1)
 	// fmt.Println(u)
@@ -29,7 +29,9 @@ func main() {
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
 
-	user, _ := models.GetUser(3)
-	user.CreateTodo("First Todo")
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("First Todo")
 
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }
